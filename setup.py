78f314efeb9b78a1f65c2fcd21b7377b397f881b
bfouts-newzoo <167096699+bfouts-newzoo@users.noreply.github.com>
2025-04-08 11:07:07 +0200

    [GPM-607] Improved login flow (#4)

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ import sys
 if not sys.version_info[0] == 3 and sys.version_info[1] < 8:
     sys.exit('Python < 3.8 is not supported')
 
-version = '1.1.2'
+version = '1.1.3'
 
 setup(
     name='steampy',
     packages=['steampy', 'test', 'examples', ],
     version=version,
-    description='A Steam lib for trade automation',
-    author='Michał Bukowski',
-    author_email='gigibukson@gmail.com',
+    description='A Steam lib for API interactions. Forked from bukson/steampy',
+    author='Brian Fouts',
+    author_email='brian.fouts@newzoo.com',
     license='MIT',
-    url='https://github.com/bukson/steampy',
-    download_url='https://github.com/bukson/steampy/tarball/' + version,
+    url='https://github.com/newzoo-nexus/steampy',
+    download_url='https://github.com/newzoo-nexus/steampy/tarball/' + version,
     keywords=['steam', 'trade', ],
     classifiers=[],
     install_requires=[
